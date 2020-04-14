5440b64557d9c4ec93ea654157703c4122a1021a
Erika Barthelmess <barthelmess@stlawu.edu>
2020-04-14 15:08:46 -0400

    add files to repo

---
+++ Lesson6/app.R
@@ -0,0 +1,62 @@
+#use R scripts and data
+
+#put calls to libraries, r scripts, etc here at the top if they only need to be called the first time you run the app.
+
+library(shiny)
+source("helpers.R")
+counties<-readRDS("data/counties.rds")
+library(maps)
+library(mapproj)
+
+# User interface ----
+ui <- fluidPage(
+  titlePanel("censusVis"),
+
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Create demographic maps with
+        information from the 2010 US Census."),
+
+      selectInput("var",
+                  label = "Choose a variable to display",
+                  choices = c("Percent White", "Percent Black",
+                              "Percent Hispanic", "Percent Asian"),
+                  selected = "Percent White"),
+
+      sliderInput("range",
+                  label = "Range of interest:",
+                  min = 0, max = 100, value = c(0, 100))
+    ),
+
+
+    mainPanel(plotOutput("map"))
+  )
+)
+
+# Server logic ----
+server <- function(input, output) {
+  output$map <- renderPlot({
+    data<-switch(input$var,
+                 "Percent White" = counties$white,
+                 "Percent Black" = counties$black,
+                 "Percent Hispanic" = counties$hispanic,
+                 "Percent Asian" = counties$asian)
+
+    color<-switch(input$var,
+                  "Percent White" = "darkgreen",
+                  "Percent Black" = "black",
+                  "Percent Hispanic" = "orange",
+                  "Percent Asian" = "red")
+
+    legend<-switch(input$var,
+                   "Percent White" = "% White",
+                   "Percent Black" = "% Black",
+                   "Percent Hispanic" = "% Hispanic",
+                   "Percent Asian" = "% Asian")
+
+  percent_map(var = data, color = color, legend.title = legend, max = input$range[2], min = input$range[1])
+  })
+}
+
+# Run app ----
+shinyApp(ui, server)
